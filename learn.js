56b06558ebb038939ee0cff74374dfe830bd9628
ISTC <ISTC@STUDY-2-4.istc.com>
2019-06-19 10:52:32 +0400

    add learn.html learn.js

---
+++ learn.js
@@ -0,0 +1,24 @@
+alert("Hello world");
+
+function f1() {
+    alert("eto js");
+}
+function f2(){
+    alert("button");
+}
+function smile() {
+    alert("this is a smile");
+}
+function pow1() {
+    alert(document.getElementById("imp1").value*document.getElementById("imp1").value);
+}
+
+var num1;
+num1 = document.getElementById("imp1").value;
+
+alert(num1*num1);
+
+num1 = 7;
+var b = 5;
+alert(num1 + b);
+
